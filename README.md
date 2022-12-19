308cb3b15ab17cfb16981e93164f602156fe5d6a
svetl <svetlana.akulowa@gmail.com>
2022-12-19 11:10:58 +0200

    add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+For admin login use:
+login - admin
+password - admin
+There's no additional configuration for DBMS, tables are created on fly in H2.
+For creating new user with "ROLE_USER" please use registration form.
--- src/main/java/homework8Gradle/homework8Gradle/security/AuthController.java
+++ src/main/java/homework8Gradle/homework8Gradle/security/AuthController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class AuthController {
