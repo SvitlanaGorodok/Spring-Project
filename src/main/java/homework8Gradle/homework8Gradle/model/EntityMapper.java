083499c61b809b7072673032843b6f6a5d4036a2
svetl <svetlana.akulowa@gmail.com>
2023-02-26 21:36:05 +0200

    add mapper interface

---
+++ src/main/java/homework8Gradle/homework8Gradle/model/EntityMapper.java
@@ -0,0 +1,16 @@
+package homework8Gradle.homework8Gradle.model;
+
+import homework8Gradle.homework8Gradle.model.dao.User;
+import homework8Gradle.homework8Gradle.model.dto.UserDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.time.LocalDate;
+
+@Mapper(componentModel = "Spring", imports = {LocalDate.class})
+public interface EntityMapper {
+
+    @Mapping(target = "passwordConfirm", ignore = true)
+    UserDto userToDto(User user);
+    User userToDAO(UserDto userDTO);
+}
