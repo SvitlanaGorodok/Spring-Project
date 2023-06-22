8fa71148878f3014a16fe43073d2eb357e6f2057
svetl <svetlana.akulowa@gmail.com>
2023-06-22 23:01:04 +0300

    start tests for product service

---
+++ src/test/java/homework8Gradle/homework8Gradle/service/ProductServiceTest.java
@@ -0,0 +1,28 @@
+package homework8Gradle.homework8Gradle.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductServiceTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void findByParameters() {
+    }
+}
