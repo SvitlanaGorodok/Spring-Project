38c9653c1b5bbaa6651b526c46effdf1a5346993
svetl <svetlana.akulowa@gmail.com>
2023-06-23 22:20:33 +0300

    start tests for user service

---
+++ src/test/java/homework8Gradle/homework8Gradle/service/UserServiceTest.java
@@ -0,0 +1,44 @@
+package homework8Gradle.homework8Gradle.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void findAllEmails() {
+    }
+
+    @Test
+    void findByEmail() {
+    }
+
+    @Test
+    void changePassword() {
+    }
+
+    @Test
+    void findByParameters() {
+    }
+}
