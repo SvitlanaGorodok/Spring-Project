8e848ba660f6a1cb7ae29300a1c1ba381d1761d4
svetl <svetlana.akulowa@gmail.com>
2023-07-03 11:39:50 +0300

    finished tests for product service

--- src/test/java/homework8Gradle/homework8Gradle/service/ProductServiceTest.java
+++ src/test/java/homework8Gradle/homework8Gradle/service/ProductServiceTest.java
@@ -1,6 +1,9 @@
 package homework8Gradle.homework8Gradle.service;
 
+import homework8Gradle.homework8Gradle.exception.NoSuchEntityFoundException;
+import homework8Gradle.homework8Gradle.model.dao.Manufacturer;
 import homework8Gradle.homework8Gradle.model.dao.Product;
+import homework8Gradle.homework8Gradle.model.dto.FindProductParam;
 import homework8Gradle.homework8Gradle.model.dto.ProductDto;
 import homework8Gradle.homework8Gradle.model.mapper.EntityMapperImpl;
 import homework8Gradle.homework8Gradle.repository.ProductRepository;
@@ -10,11 +13,14 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
 
 class ProductServiceTest {
 
@@ -58,18 +64,115 @@ class ProductServiceTest {
     }
 
     @Test
+    void saveWithNullId() {
+        when(repository.save(any(Product.class))).thenAnswer(i -> i.getArguments()[0]);
+        ProductDto toSave = new ProductDto();
+        assertNull(toSave.getId());
+
+        ProductDto saved = service.save(toSave);
+        assertNotNull(saved.getId());
+
+    }
+
+    @Test
     void findAll() {
+
+        UUID productId = UUID.randomUUID();
+        UUID manufacturerId = UUID.randomUUID();
+        ProductDto productDto = new ProductDto();
+        productDto.setId(productId);
+        productDto.setName("name");
+        productDto.setPrice(0L);
+        productDto.setManufacturerId(manufacturerId);
+
+        Product product = new Product();
+        Manufacturer manufacturer = new Manufacturer();
+        manufacturer.setId(manufacturerId);
+        product.setId(productId);
+        product.setName("name");
+        product.setPrice(0L);
+        product.setManufacturer(manufacturer);
+
+        when(repository.findAll()).thenReturn(List.of(product));
+
+        List<ProductDto> productDtos = service.findAll();
+
+        assertEquals(productDtos.get(0).getId(), productDto.getId());
+        assertEquals(productDtos.get(0).getName(), productDto.getName());
+        assertEquals(productDtos.get(0).getPrice(), productDto.getPrice());
+        assertEquals(productDtos.get(0).getManufacturerId(), productDto.getManufacturerId());
+
     }
 
     @Test
     void findById() {
+        UUID productId = UUID.randomUUID();
+        UUID manufacturerId = UUID.randomUUID();
+
+        Product product = new Product();
+        Manufacturer manufacturer = new Manufacturer();
+        manufacturer.setId(manufacturerId);
+        product.setId(productId);
+        product.setName("name");
+        product.setPrice(0L);
+        product.setManufacturer(manufacturer);
+
+        when(repository.findById(any(UUID.class))).thenReturn(Optional.of(product));
+
+        ProductDto productDto = service.findById(productId);
+
+        assertEquals(productId, productDto.getId());
+    }
+
+    @Test
+    void findByIdNoSuchEntityFoundException() {
+        UUID id = UUID.randomUUID();
+        when(repository.findById(any(UUID.class))).thenReturn(Optional.empty());
+        NoSuchEntityFoundException exception = assertThrows(NoSuchEntityFoundException.class,
+                () -> service.findById(id));
+        assertEquals("Product with id " + id + "not found!", exception.getMessage());
     }
 
     @Test
     void deleteById() {
+        UUID id = UUID.randomUUID();
+        doNothing().when(repository).deleteById(any(UUID.class));
+        service.deleteById(id);
+        verify(repository,times(1)).deleteById(any(UUID.class));
     }
 
     @Test
     void findByParameters() {
+        FindProductParam productParam = new FindProductParam();
+        UUID manufacturerId = UUID.randomUUID();
+        productParam.setName("name");
+        productParam.setUnderPrice(3L);
+        productParam.setOverPrice(1L);
+        productParam.setManufacturerId(manufacturerId);
+
+        UUID productId = UUID.randomUUID();
+        ProductDto productDto = new ProductDto();
+        productDto.setId(productId);
+        productDto.setName("name");
+        productDto.setPrice(2L);
+        productDto.setManufacturerId(manufacturerId);
+
+        Product product = new Product();
+        Manufacturer manufacturer = new Manufacturer();
+        manufacturer.setId(manufacturerId);
+        product.setId(productId);
+        product.setName("name");
+        product.setPrice(2L);
+        product.setManufacturer(manufacturer);
+
+        when(repository.findByParameters(anyString(), anyLong(), anyLong(), any(UUID.class)))
+                .thenReturn(List.of(product));
+
+        ProductDto findProduct = service.findByParameters(productParam).get(0);
+
+        assertEquals(findProduct.getName(), productParam.getName());
+        assertEquals(findProduct.getManufacturerId(), productParam.getManufacturerId());
+        assertTrue(findProduct.getPrice() > productParam.getOverPrice());
+        assertTrue(findProduct.getPrice() < productParam.getUnderPrice());
     }
 }
